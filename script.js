02cd1ee01e731889c0eb7cd2e3191a4ab3c5ca09
okoro <mi.fracri91@gmail.com>
2023-11-11 20:42:21 +0100

    completed script functions and logic

--- script.js
+++ script.js
@@ -1,123 +1,122 @@
-
-
 const container = document.querySelector('#container');
+const gridSize = document.querySelector('#grid-size');
+const gridText = document.querySelector('#grid-text');
 
+gridSize.addEventListener('input', updateGridSize);
+const showValue = gridSize.value;
+gridText.textContent = `${showValue} x ${showValue}`;
 
+function sketchGrid(gridCell) {
+  container.innerHTML = ''; 
 
-function sketchGrid(gridCell){
-    
-    
+  for (let i = 0; i < gridCell * gridCell; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('grid-cell');
+    container.appendChild(cell);
+  }
 
-for (let i = 0; i < gridCell * gridCell; i++){
-        const cell = document.createElement('div');
-        cell.classList.add('grid-cell');
-        container.appendChild(cell);
+  container.style.gridTemplateColumns = `repeat(${gridCell}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridCell}, 1fr)`;
+
+  const cells = document.querySelectorAll('.grid-cell');
+  cellColor(cells);
 }
 
-}
 
-
-
-
-
-
-sketchGrid(16);
-
-
-
-
-
-
-
-const cells = document.querySelectorAll('.grid-cell');
-
-//rain color created here
+//rainbow color created here
 function rainbow(event) {
-        const R = Math.floor(Math.random() * 256);
-        const G = Math.floor(Math.random() * 256);
-        const B = Math.floor(Math.random() * 256);
-        event.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
+    const R = Math.floor(Math.random() * 256);
+    const G = Math.floor(Math.random() * 256);
+    const B = Math.floor(Math.random() * 256);
+    event.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
 }
 
 // other color choice created here
 
 const colorChoice = document.querySelector('#color-wheel');
 let colorPicker = (event) => {
-    event.target.style.backgroundColor = colorChoice.value;
+event.target.style.backgroundColor = colorChoice.value;
 }
 
 // eraser cell 
 
 let eraser = (event) => {
-    event.target.style.backgroundColor = '#d3e4e3';
+event.target.style.backgroundColor = '#d3e4e3';
 }
 
 
+function cellColor(cells) {
+  const activeButton = document.querySelector('.active');
 
+  // Remove existing event listeners from all cells
+  cells.forEach(cell => {
+    cell.removeEventListener('mouseover', colorPicker);
+    cell.removeEventListener('mouseover', rainbow);
+    cell.removeEventListener('mouseover', eraser);
+  });
 
+  // Add new event listener based on the active button
+  if (activeButton && activeButton.id === 'color-change') {
+    cells.forEach(cell => {
+      cell.addEventListener('mouseover', colorPicker);
+    });
+  } else if (activeButton && activeButton.id === 'rainbow') {
+    cells.forEach(cell => {
+      cell.addEventListener('mouseover', rainbow);
+    });
+  } else if (activeButton && activeButton.id === 'erase') {
+    cells.forEach(cell => {
+      cell.addEventListener('mouseover', eraser);
+    });
+  }
+}
+
+function updateGridSize() {
+  const newValue = gridSize.value;
+  gridText.textContent = newValue;
+  sketchGrid(newValue);
+}
+
+sketchGrid(16);
+
+// Color buttons and event listeners
 const rainbowButton = document.querySelector('#rainbow');
 const colorButton = document.querySelector('#color-change');
 const eraseButton = document.querySelector('#erase');
 
-
-rainbowButton.addEventListener('click', () => {
-  rainbowButton.classList.add('active');
-  colorButton.classList.remove('active');
+colorButton.addEventListener('click', () => {
+  colorButton.classList.add('active');
+  rainbowButton.classList.remove('active');
   eraseButton.classList.remove('active');
-  cellColor();
+  const cells = document.querySelectorAll('.grid-cell'); 
+  cellColor(cells);
 });
 
-colorButton.addEventListener('click', () => {
-  rainbowButton.classList.remove('active');
-  colorButton.classList.add('active');
+rainbowButton.addEventListener('click', () => {
+  colorButton.classList.remove('active');
+  rainbowButton.classList.add('active');
   eraseButton.classList.remove('active');
-  cellColor();
+  const cells = document.querySelectorAll('.grid-cell'); 
+  cellColor(cells);
 });
 
 eraseButton.addEventListener('click', () => {
   rainbowButton.classList.remove('active');
   colorButton.classList.remove('active');
   eraseButton.classList.add('active');
-  cellColor();
+  const cells = document.querySelectorAll('.grid-cell'); 
+  cellColor(cells);
 });
 
-
-const cellColor = function () {
-    const activeButton = document.querySelector('.active');
-
-    // Remove existing event listeners from all cells
-    cells.forEach(cell => {
-        cell.removeEventListener('mouseover', colorPicker);
-        cell.removeEventListener('mouseover', rainbow);
-        cell.removeEventListener('mouseover', eraser);
-    });
-
-    // Add new event listener based on the active button
-    if (activeButton && activeButton.id === 'color-change') {
-        cells.forEach(cell => {
-            cell.addEventListener('mouseover', colorPicker);
-        });
-    } else if (activeButton && activeButton.id === 'rainbow') {
-        cells.forEach(cell => {
-            cell.addEventListener('mouseover', rainbow);
-        });
-    } else if (activeButton && activeButton.id === 'erase') {
-        cells.forEach(cell => {
-            cell.addEventListener('mouseover', eraser);
-        });
-    }
-}
-
-
-
-// clear all cell function
+// Clear button and event listener
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', () => {
-    clear();
-  });
+  clear();
+});
 
-let clear = (event) => {
-    cells.forEach(cell => {
-        cell.style.backgroundColor = ''; 
-      });
+function clear() {
+  const cells = document.querySelectorAll('.grid-cell');
+  cells.forEach(cell => {
+    cell.style.backgroundColor = '';
+  });
 }
