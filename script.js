047338126f37aba26ad9fe806c52a5b174be4de2
okoro <mi.fracri91@gmail.com>
2023-11-11 06:29:09 +0100

    added rainbow color function

--- script.js
+++ script.js
@@ -13,6 +13,19 @@ for (let i = 0; i < rows * cols; i++){
 
 sketchGrid(16, 16);
 
+const cells = document.querySelectorAll('.grid-cell');
+const colorChoice = document.querySelector('#colorWheel');
+
+function rainbow(event) {
+        const R = Math.floor(Math.random() * 256);
+        const G = Math.floor(Math.random() * 256);
+        const B = Math.floor(Math.random() * 256);
+        event.target.style.backgroundColor = `rgb(${R}, ${G}, ${B})`
+}
+
+cells.forEach(cell => {
+    cell.addEventListener('mouseover', rainbow);
+});
 
 
 
